2ecc834a1727f7201c6f7c4b6b20a39e445cbb93
AlanDaniel500 <alan23102003@gmail.com>
2025-08-06 12:44:13 -0300

    Movimiento basico

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,26 +5,59 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody2D rb;
-    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private PlayerInput playerInput;
+
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float jumpForce = 5f;
 
 
+    public bool isGrounded;
     float horizontalMovement;
 
-    void Start()
+    void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.linearVelocity = new Vector2(horizontalMovement * moveSpeed, rb.linearVelocity.y);
+       rb.linearVelocity = new Vector2(horizontalMovement * moveSpeed, rb.linearVelocity.y);
     }
 
 
     public void Move(InputAction.CallbackContext context)
     {
-        horizontalMovement = context.ReadValue<float>();
+        Vector2 inputVector = context.ReadValue<Vector2>();
+
+        rb.linearVelocity = new Vector2(inputVector.x * moveSpeed, rb.linearVelocity.y);
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed && isGrounded)
+        {
+            Debug.Log("Jump pressed");
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
     }
 }
